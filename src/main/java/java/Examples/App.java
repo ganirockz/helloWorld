a4363e6628e81fbd439c666a2b17cb2691af7565
ganirockz <ganeshrapeti8@gmail.com>
2020-09-21 14:16:08 +0530

    first commit

---
+++ src/main/java/java/Examples/App.java
@@ -0,0 +1,24 @@
+package java.Examples;
+
+import org.apache.logging.log4j.*;
+
+//import jdk.internal.org.jline.utils.Log;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	String message = "Hi Good Morning";
+    	LOG.debug(message+" Debug");
+    	LOG.warn(message+" warn");
+    	LOG.info(message+" info");
+    	LOG.error( message+" Error");
+    	LOG.fatal( message+" fatal");
+    	LOG.info("Appending string: {}.",message);
+    	System.out.println(message);
+    }
+}
